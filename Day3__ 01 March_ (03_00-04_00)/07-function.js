7b45811b4678a22ff77504737f0aaca31193bce5
DIPU SARDAR <xeedipu@gmail.com>
2025-03-02 01:29:52 +0600

    Day Three of learning javascript.

---
+++ Day3__ 01 March_ (03_00-04_00)/07-function.js
@@ -0,0 +1,9 @@
+// If we want to make a function then this is the basic structure of a function. 
+
+function addTwoNumbers(){
+    console.log(2+5);
+}
+
+// Now I need to call the function to work properly this function. 
+
+addTwoNumbers();
---
+++ Day3__ 01 March_ (03_00-04_00)/08-Parameter.js
@@ -0,0 +1,22 @@
+// If we want to make a function then this is the basic structure of a function. 
+
+/* function addTwoNumbers(){
+    console.log(2+5);
+}
+*/
+// Now I need to call the function to work properly this function. 
+
+// addTwoNumbers();
+
+
+// If we want to give a value in a function this is called parameter 
+
+// If we want to make a function then this is the basic structure of a function. 
+
+function addTwoNumbers(num1,num2){
+    console.log(num1+num2);
+}
+
+// Now I need to call the function to work properly this function. 
+
+addTwoNumbers(5,10);
