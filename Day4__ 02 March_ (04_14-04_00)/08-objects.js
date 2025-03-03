71baac74a18b9202ad71331c9754d59389704a2d
DIPU SARDAR <xeedipu@gmail.com>
2025-03-03 07:34:08 +0600

    Add files via upload

---
+++ Day4__ 02 March_ (04_14-04_00)/08-objects.js
@@ -0,0 +1,22 @@
+// Let's name an onjects on Javascript.
+let girlfriendNames = {
+    name1 : 'Mahfuza',
+    age1 : 22,
+    name2 : 'Ana de',
+    age2 : 34
+}
+
+console.log(girlfriendNames.age1);
+console.log(girlfriendNames.name2);
+
+// I can add properties to apply this
+
+girlfriendNames.gender = 'male';
+
+console.log(girlfriendNames);
+
+// I can delete properties by applying this
+
+delete girlfriendNames.name1;
+
+console.log(girlfriendNames);
